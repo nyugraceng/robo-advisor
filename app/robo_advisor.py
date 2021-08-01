811c4b7d4184904ed3943b2e51d4bc548052aec6
nyugraceng <87045641+nyugraceng@users.noreply.github.com>
2021-07-31 22:10:21 -0400

    Get latest close in USD

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -3,49 +3,41 @@ import json
 
 from pprint import pprint
 
+def to_usd(my_price):
+    return "${0:,.2f}".format(my_price)
+
+symbol = "XYZ"
 request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo"
+
 response = requests.get(request_url)
-print(type(response))
-print(response.status_code)
-print(response.text)
+# print(type(response))
+# print(response.status_code)
+# print(response.text)
 
 parsed_response = json.loads(response.text)
 
 last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
 
+latest_close = parsed_response["Time Series (Daily)"]["2021-07-30"]["4. close"] #> $100.00
 
-breakpoint()
-
-quit()
+#breakpoint()
 
 #from getpass import getpass # secret version of the import function
 # locally we'll wan t to use env vars
 # but on colab we can use getpass / input them
 #api_key = getpass("Please input your api key:")
 
-import os
-from dotenv import load_dotenv
-load_dotenv() #> loads contents of the .env file into the script's environment
-api_key = os.getenv("ALPHAVANTAGE_API_KEY")
-
-
-symbol = "MSFT"
-stock_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={api_key}"
-response = requests.get(stock_url)
-parsed_response = json.loads(response.text)
-print(type(parsed_response))
-pprint(parsed_response)
-
 # this is the "app/robo_advisor.py" file
 
+
 print("-------------------------")
-print("SELECTED SYMBOL: XYZ")
+print("SELECTED SYMBOL: IBM")
 print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
-print("REQUEST AT: 2018-02-20 02:00pm")
+print("REQUEST AT: 2018-02-20 02:00pm") #use day time module
 print("-------------------------")
 print(f"LATEST DAY: {last_refreshed}")
-print("LATEST CLOSE: $100,000.00")
+print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
 print("-------------------------")
