1ca5d081c7598a88139a7c3c6f86740ff2375380
nyugraceng <87045641+nyugraceng@users.noreply.github.com>
2021-08-02 00:49:22 -0400

    Final Commit with Recommendation and Output Requirements

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # robo-advisor
+
+Overview 
+This is the Robo Advisor project for our Python class: https://github.com/prof-rossetti/intro-to-python/blob/main/projects/robo-advisor/README.md 
+In essence, the output allows the user to pull stock data from an API at a certain point in time to be able to see the latest close, recent high, recent low, recommendation and why of a stock. 
+
+Instructions
+You can clone my Repo from: https://github.com/nyugraceng/robo-advisor 
+
+You will need to choose a stock ticker API: https://www.alphavantage.co/documentation/. We will be using the Time Series Daily API. 
+
+Packages
+You will need to install certain packages in order to proceed:
+1. CSV 
+2. JSON
+3. OS 
+4. pandas
+5. pip install requirements 
+
+Files and functions 
+You will need an environment file (.env) to store your secret API key, obtainable for free from alphavantage website. Copy and paste the key, defined "ALPHAVANTAGE_API_KEY="inputyourAPIkey". By defining it, you can then use the os package to get the script to obtain this key from your environment. 
+
+Please ensure you create a gitignore file and a read me file when you create your own repository. This ensures that your personal API key will not be uploaded onto GitHub.
+
+You will need to use the to_usd function to format prices. 
+
+Data CSV file 
+You will need to create a data folder and a csv file to store the data. 
--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,86 +1,105 @@
-
 import csv
 import requests
+from datetime import datetime
 import json
 import os
 
+from dotenv import load_dotenv
+load_dotenv()
+
 from pprint import pprint
 
-def to_usd(my_price):
-    return "${0:,.2f}".format(my_price)
+def run_automated_stock_rec(symbol):
+    api_key = os.environ.get("ALPHAVANTAGE_API_KEY")
 
-symbol = "XYZ"
-request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo"
+    def to_usd(my_price):
+        return "${0:,.2f}".format(my_price)
 
-response = requests.get(request_url)
-# print(type(response))
-# print(response.status_code)
-# print(response.text)
+    request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={api_key}"
 
-parsed_response = json.loads(response.text)
+    response = requests.get(request_url)
+    parsed_response = json.loads(response.text)
+    # pprint(parsed_response)
+    try:
+        last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+    except KeyError:
+        print("Uh oh, this seems to be an invalid symbol - maybe try again?")
+        return
 
-last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+    tsd = parsed_response["Time Series (Daily)"]
 
-tsd = parsed_response["Time Series (Daily)"]
+    # sort to ensure latest day is first 
+    dates = list(tsd.keys()) 
 
-# sort to ensure latest day is first 
-dates = list(tsd.keys()) 
+    latest_day = dates[0] 
+    latest_close = float(tsd[latest_day]["4. close"])
 
-latest_day = dates[0] 
+    high_prices = []
+    low_prices = []
 
-latest_close = tsd[latest_day]["4. close"] 
-
-# getting high price from each day
-
-high_prices = []
-low_prices = []
-
-for date in dates:
-    high_price = tsd[date]["2. high"]
-    high_prices.append(float(high_price))
-    low_price = tsd[date]["3. low"]
-    low_prices.append(float(low_price))
-
-# max of all the high prices 
-#high_prices = [10, 20, 30, 5]
-
-recent_high = max(high_prices) 
-recent_low = min(low_prices) 
-
-# csv_file_path = "data/prices.csv" change to be OS friendly
-csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", "prices.csv")
-
-csv_headers = ["timestamp", "open", "high", "low", "close", "volume"]
-
-with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writing"
-    writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
-    writer.writeheader() # uses fieldnames set above
     for date in dates:
-        daily_prices = tsd[date]
-        writer.writerow({
-            "timestamp": date,
-            "open": daily_prices["1. open"],
-            "high": daily_prices["2. high"],
-            "low": daily_prices["3. low"],
-            "close": daily_prices["4. close"],
-            "volume": daily_prices["5. volume"]
-        })
+        high_price = tsd[date]["2. high"]
+        high_prices.append(float(high_price))
+        low_price = tsd[date]["3. low"]
+        low_prices.append(float(low_price))
 
-print("-------------------------")
-print("SELECTED SYMBOL: IBM")
-print("-------------------------")
-print("REQUESTING STOCK MARKET DATA...")
-print("REQUEST AT: 2018-02-20 02:00pm") 
-print("-------------------------")
-print(f"LATEST DAY: {last_refreshed}")
-print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
-print(f"RECENT HIGH: {to_usd(float(recent_high))}")
-print(f"RECENT LOW: {to_usd(float(recent_low))}")
-print("-------------------------")
-print("RECOMMENDATION: BUY!")
-print("RECOMMENDATION REASON: TODO")
-print("-------------------------")
-print(f"WRITING DATA TO CSV: {csv_file_path }..")
-print("-------------------------")
-print("HAPPY INVESTING!")
-print("-------------------------")
+
+    recent_high = float(max(high_prices))
+    recent_low = float(min(low_prices))
+
+    # csv_file_path = "data/prices.csv" change to be OS friendly
+    csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", "prices.csv")
+
+    csv_headers = ["timestamp", "open", "high", "low", "close", "volume"]
+
+    with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writing"
+        writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
+        writer.writeheader() # uses fieldnames set above
+        for date in dates:
+            daily_prices = tsd[date]
+            writer.writerow({
+                "timestamp": date,
+                "open": daily_prices["1. open"],
+                "high": daily_prices["2. high"],
+                "low": daily_prices["3. low"],
+                "close": daily_prices["4. close"],
+                "volume": daily_prices["6. volume"]
+            })
+
+    now = datetime.now()
+    current_time = now.strftime("%I:%M:%S")
+    currentSecond= datetime.now().second
+    currentMinute = datetime.now().minute
+    currentHour = datetime.now().hour
+
+    current_date = now.strftime("%d %B,%Y")
+    currentDay = datetime.now().day
+    currentMonth = datetime.now().month
+    currentYear = datetime.now().year
+
+    # recommendation
+    is_buy = "BUY" if latest_close < recent_low * 1.2 else "DON'T BUY"
+    rec_condition = "less than" if is_buy == "BUY" else "greater than"
+
+    print("-------------------------")
+    print(f"SELECTED SYMBOL: {symbol}")
+    print("-------------------------")
+    print("REQUESTING STOCK MARKET DATA...")
+    # Run at: 11:52pm on June 5th, 2018
+    print(f"YOUR REQUEST RUN AT: {current_time} on {current_date}") 
+    print("-------------------------")
+    print(f"LATEST DAY: {last_refreshed}")
+    print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
+    print(f"RECENT HIGH: {to_usd(float(recent_high))}")
+    print(f"RECENT LOW: {to_usd(float(recent_low))}")
+    print("-------------------------")
+    print(f"RECOMMENDATION: {is_buy}")
+    print(f"RECOMMENDATION REASON: We are recommending {is_buy.lower()} {symbol} because it's latest closing price {to_usd(latest_close)} is {rec_condition} 20% above its recent low {to_usd(recent_low)}")
+    print("-------------------------")
+    print(f"WRITING DATA TO CSV: {csv_file_path }...")
+    print("-------------------------")
+    print("HAPPY INVESTING!")
+    print("-------------------------")
+
+symbol = "TSLA"
+run_automated_stock_rec(symbol) 
