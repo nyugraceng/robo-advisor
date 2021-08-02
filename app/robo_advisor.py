05bd08b496384de61dff6adb68c58e81cee11a5e
nyugraceng <87045641+nyugraceng@users.noreply.github.com>
2021-08-01 20:07:02 -0400

    Write All Daily Prices to CSV

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -56,15 +56,15 @@ with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writin
     writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
     writer.writeheader() # uses fieldnames set above
     for date in dates:
+        daily_prices = tsd[date]
         writer.writerow({
             "timestamp": date,
-            "open": "TODO",
-            "high": "TODO",
-            "low": "TODO",
-            "close": "TODO",
-            "volume": "TODO"
+            "open": daily_prices["1. open"],
+            "high": daily_prices["2. high"],
+            "low": daily_prices["3. low"],
+            "close": daily_prices["4. close"],
+            "volume": daily_prices["5. volume"]
         })
-
 
 print("-------------------------")
 print("SELECTED SYMBOL: IBM")
